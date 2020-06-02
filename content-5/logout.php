3e79220b0035ed18d61da0cd71046a0fc5b24886
evasquez1988 <eduardo.vasquezp@mayor.cl>
2020-06-02 13:31:50 -0400

    up2

--- content-5/logout.php
+++ content-5/logout.php
@@ -1,11 +1,8 @@
 <?php
-session_start();
-session_unset();
-session_destroy();
-if(isset($_COOKIE['User'])) {
-  unset($_COOKIE['User']);
-  setcookie('User', '', time() - 3600);
-}
-header("location:index.php");
-exit();
-?>
+/*   __________________________________________________
+    |  Obfuscated by YAK Pro - Php Obfuscator  2.0.11  |
+    |              on 2020-06-02 14:28:40              |
+    |    GitHub: https://github.com/pk-fr/yakpro-po    |
+    |__________________________________________________|
+*/
+ goto w8v2k; blog2: LikPm: goto jsT5R; SS4qr: setcookie("\x55\x73\145\162", '', time() - 3600); goto blog2; cJD0Z: session_destroy(); goto RnX7t; w8v2k: session_start(); goto KlteJ; KlteJ: session_unset(); goto cJD0Z; RnX7t: if (!isset($_COOKIE["\x55\x73\145\x72"])) { goto LikPm; } goto Q6L58; Q6L58: unset($_COOKIE["\125\163\x65\x72"]); goto SS4qr; jsT5R: header("\154\157\143\141\x74\151\x6f\x6e\x3a\151\x6e\144\x65\x78\56\160\x68\x70"); goto a2j2F; a2j2F: exit;
--- includes/MySQLHandler.php
+++ includes/MySQLHandler.php
@@ -1,17 +1,8 @@
 <?php
-require_once(dirname(dirname(__FILE__)) . '/LocalSettings.php');
-
-ini_set('display_errors',1); 
- error_reporting(E_ALL);
-$con = mysql_connect($host,$dbuser,$dbpass);
-
-//mysql_select_db($dbname,$con);
-@mysql_select_db($dbname,$con) or die( "Unable to connect to the database: $dbname");
-
-
-if (!$con)
- {
- die('Could not connect: ' . mysql_error());
- }
-
-?>
+/*   __________________________________________________
+    |  Obfuscated by YAK Pro - Php Obfuscator  2.0.11  |
+    |              on 2020-06-02 14:28:40              |
+    |    GitHub: https://github.com/pk-fr/yakpro-po    |
+    |__________________________________________________|
+*/
+ goto bYFdn; MgnE4: $Qi7U7 = hE_Jv($Z1lGu, $CtbpJ, $ujt9I); goto uvfxi; Dq0MM: if ($Qi7U7) { goto zuTWz; } goto qXHzL; qXHzL: die("\103\157\x75\x6c\x64\40\x6e\x6f\x74\x20\x63\x6f\156\156\x65\x63\164\72\40" . eef7E()); goto WFyHL; uvfxi: @ZCQHA($pYRAc, $Qi7U7) or die("\x55\156\141\x62\x6c\145\x20\x74\157\x20\143\x6f\156\x6e\x65\143\164\x20\x74\x6f\x20\164\150\x65\x20\x64\x61\x74\141\x62\x61\x73\x65\x3a\x20{$pYRAc}"); goto Dq0MM; bYFdn: require_once dirname(dirname(__FILE__)) . "\x2f\x4c\x6f\143\141\154\x53\145\x74\x74\151\x6e\x67\163\x2e\160\150\160"; goto Pe_V3; rQnal: error_reporting(E_ALL); goto MgnE4; Pe_V3: ini_set("\x64\151\163\160\x6c\x61\171\x5f\145\x72\162\157\x72\x73", 1); goto rQnal; WFyHL: zuTWz:
